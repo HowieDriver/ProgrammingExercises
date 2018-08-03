6881b842227482ae41b114c807dcd12109112101
Howie <yong_zhang@163.com>
2018-08-03 15:41:18 +0800

    Add new file reconstructBinTree
    Reconstruction of two forked trees.
    前序遍历的特点（根-左子树-右子树），中序遍历的特点（左子树-根-右子树）
    前序遍历每次读取的第一个值一定是根节点，这样我们可以在中序遍历的序列中找到当前
    的根节点的位置。中序遍历当确定了一个根节点后，其左边序列就是这个根节点的左子树
    ，右边序列就是其右子树。
    
    Signed-off-by: Howie <yong_zhang@163.com>

--- reconstructBinTree.cpp
+++ reconstructBinTree.cpp
@@ -1,1 +1,40 @@
-sfdsdsd
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* reConstructBinaryTree(vector<int> pre,vector<int> vin) {
+        int plen = pre.size();
+        int vlen = vin.size();
+        if(plen<1 || vlen <1)
+            return NULL;
+        vector<int> vleft, vright;
+        vector<int> pleft, pright;
+        int index=0;
+        for(int i=0; i<plen; ++i) {
+            if(vin[i] == pre[0]) {
+                index = i;
+                break;
+            }
+        }
+        TreeNode *node = new TreeNode(pre[0]);
+        
+        for(int i=0; i<index; ++i) {
+            vleft.push_back(vin[i]);
+            pleft.push_back(pre[i+1]);
+        }
+        for(int i=index+1; i<vlen; ++i) {
+            vright.push_back(vin[i]);
+            pright.push_back(pre[i]);
+        }
+        node->left = reConstructBinaryTree(pleft, vleft);
+        node->right = reConstructBinaryTree(pright, vright);
+        return node;
+    }
+};
